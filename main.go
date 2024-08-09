6a325ba21ba008b108cddb7b2bfc6fd70c1ae914
nothing-c <halskdjf@gmail.com>
2024-08-09 18:54:09 -0500

    Data acquisition, cleaning, and moer scaffolding

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 
 import (
-
+    "os"
+    "io"
+    "strings"
 )
 
+var H [string][[]string]
+
 func main() {
-    I:="testdata.txt"
-
-
+    I:="testdata-clean.txt"
+    F:=os.Open(I)
+    C,e:=io.ReadAll(F); if e!=nil{ panic(e) }
+    // structural regexes could come in handy here
+    for _,x := range strings.Split(C,"\n") {
+        
+    }
 }
