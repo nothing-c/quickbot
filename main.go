01b14521a0834fb62389e2ad350f958067be3a07
nothing-c <halskdjf@gmail.com>
2024-08-09 19:27:28 -0500

    Basic datastructures + 1st pass down

--- main.go
+++ main.go
@@ -5,16 +5,31 @@ import (
     "os"
     "io"
     "strings"
+    "slices"
+    "fmt"
 )
 
-var H [string][[]string]
+var H map[string][]string //Hash
+var U []string //Unique; possibly unnecessary, but I'll keep using it for now
 
 func main() {
+    H = make(map[string][]string) // Init hash
     I:="testdata-clean.txt"
-    F:=os.Open(I)
+    F,e:=os.Open(I); if e!=nil{ panic(e) }
     C,e:=io.ReadAll(F); if e!=nil{ panic(e) }
-    // structural regexes could come in handy here
-    for _,x := range strings.Split(C,"\n") {
-        
+    // structural regexes could come in handy here, or bufio.ScanWords
+    for _,x := range strings.Split(string(C),"\n") {
+        for _,y:= range strings.Split(x," ") {
+            if y == "" { continue } //skip unused
+            if slices.Contains(U, y) { //ditto
+                continue
+            } else {
+                U=append(U,y)
+                H[y]=[]string{"<abend>"}
+            }
+        }
     }
+    fmt.Println(U)
+    fmt.Println(H)
+    fmt.Println("~~~~"+U[1])
 }
