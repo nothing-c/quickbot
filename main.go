9aeca2b952f0f6931476f2defdfdd0905ae9780b
nothing-c <halskdjf@gmail.com>
2024-08-09 19:39:18 -0500

    More data fixxation (Windows sucks), next datastructure finished

--- main.go
+++ main.go
@@ -11,15 +11,17 @@ import (
 
 var H map[string][]string //Hash
 var U []string //Unique; possibly unnecessary, but I'll keep using it for now
+var D [][]string //Data; [word1, word2] ad nausueam
 
 func main() {
     H = make(map[string][]string) // Init hash
-    I:="testdata-clean.txt"
+    I:="testnew.txt"
     F,e:=os.Open(I); if e!=nil{ panic(e) }
     C,e:=io.ReadAll(F); if e!=nil{ panic(e) }
     // structural regexes could come in handy here, or bufio.ScanWords
     for _,x := range strings.Split(string(C),"\n") {
-        for _,y:= range strings.Split(x," ") {
+        for i,y:= range strings.Split(x," ") {
+            if i != len(strings.Split(x," "))-1 { t:=strings.Split(x," ")[i:i+2]; D=append(D,t) }
             if y == "" { continue } //skip unused
             if slices.Contains(U, y) { //ditto
                 continue
@@ -29,7 +31,8 @@ func main() {
             }
         }
     }
-    fmt.Println(U)
-    fmt.Println(H)
-    fmt.Println("~~~~"+U[1])
+//    fmt.Println(U)
+//    fmt.Println(H)
+//    fmt.Println("~~~~"+U[1])
+//    fmt.Println(D)
 }
