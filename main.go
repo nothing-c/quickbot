f9996eaec31607e88e8f8e7b6ef7ed280f88ef6d
nothing-c <halskdjf@gmail.com>
2024-08-12 15:57:12 -0500

    Refactoring + adding help messages and commandline switches

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
     "crypto/rand"
     "math/big"
     "encoding/json"
+    "flag"
 )
 
+    
 var H map[string][]string //Hash, output to JSON
 var U []string //Unique; possibly unnecessary, but I'll keep using it for now
 var D [][]string //Data; [word1, word2] ad nausueam
@@ -30,19 +32,29 @@ func pullweights(I string) map[string][]string {
     return R
 }
 
-func run(S string) string {
+func run(S string, H map[string][]string) string {
     if !slices.Contains(U,S) { return "<abend>" }
     if len(H[S]) == 1 { return H[S][0] } // Otherwise rand.Int freaks
     n,e:=rand.Int(rand.Reader,big.NewInt(int64(len(H[S])-1))); if e!=nil{ panic(e) } // rand is very finicky
     return H[S][n.Int64()]
 }
 
-func main() {
+func help () {
+    A := []string{"h","t","r","o","w","u","l"}
+    fmt.Println("Quickbot: Markov bot training and usage in a small, fast package ")
+    fmt.Println("Usage: qbot [-hul] [-t infile [-o outfile]] [-r [-w weightfile]]")
+    for _,x := range A {
+        fmt.Println("-" + x + ": " + flag.Lookup(x).Usage)
+    }
+    fmt.Println("If no options are provided, qbot will simply print this message and exit")
+    os.Exit(0)
+}
+
+func train(I string, Up bool) {
+    var E string // Escrow
     H = make(map[string][]string) // Init hash
-    I:="testnew.txt"
     F,e:=os.Open(I); if e!=nil{ panic(e) }
     C,e:=io.ReadAll(F); if e!=nil{ panic(e) }
-    var E string // Escrow
     // structural regexes could come in handy here, or bufio.ScanWords, but I /do/ want to buffer by lines
     for _,x := range strings.Split(string(C),"\n") {
         for i,y:= range strings.Split(x," ") {
@@ -64,15 +76,44 @@ func main() {
         k:=strings.ToLower(x[0]) // Key
         v:=strings.ToLower(x[1]) // Value
         //We can confidently assume that all of the words here will be in H
-        if slices.Contains(H[k], v) { continue } // I /could/ take this out and have some natural weighting occur; maybe a cmdline option?
+        if Up && slices.Contains(H[k], v) { continue } // Unweighted training
         H[k]=append(H[k],v)
     }
-    Q := "so"
+}
+
+func interact(L int) {
+    fmt.Println(L)
+    //Not doing the full thing yet
+    fmt.Println(H)
+    Q := "in";
     fmt.Println(Q)
     for Q != "<abend>" {
-        Q = run(Q)
-        fmt.Println(Q)
-    }
+        Q = run(Q,H)
+        fmt.Println(Q)}
+}
+
+func main() {
+    Ot:=flag.Bool("t",false,"Train only (outputting to \"weights.json\" if the output file is not specified)")
+    Or:=flag.Bool("r",false,"Run only (using \"weights.json\" if the weight file is not specified)")
+    Ow:=flag.String("w","","Specify weight file")
+    Oo:=flag.String("o","","Specify output file")
+    Ou:=flag.Bool("u",false,"Enable \"unweighted\" training; weights only contain distinct words, with no information about how often the occur")
+    Ol:=flag.Int("l",0,"Specify minimum response length")
+    Oh:=flag.Bool("h",false,"Display this message")
+    flag.Parse()
+    if *Oh==true { help() }
+    if *Ot==true { if *Oo != "" { train(flag.Arg(0),*Ou); dumpweights(H,*Oo) } else { train(flag.Arg(0),*Ou); dumpweights(H,"weights.json") } 
+        os.Exit(0)}
+    if *Or==true { if *Ow != "" { H = pullweights(*Ow); interact(*Ol) } else { H = pullweights("weightfile.json"); interact(*Ol) } 
+        os.Exit(0) }
+    help()
+    // I:="testnew.txt"
+    // Q := "so"
+    // fmt.Println(Q)
+    // for Q != "<abend>" {
+    //     Q = run(Q,H)
+    //     fmt.Println(Q)
+    // }
     // Example: dumpweights(H,"weightfile.json")
     // Example: fmt.Println(pullweights("weightfile.json"))
 }
